488a458dc586bb6b43b0dee311643e58152923f4
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-03 13:56:39 +0530

    added d1.sh

--- d1.sh
+++ d1.sh
@@ -5,23 +5,25 @@ hourlywage=20;
 salary=0;
 count=0;
 day=0
-twhours=0;
-
+twhours=0
 for (( count=0; count<=10; count++ ))
 do
-   status=$(($RANDOM%2))
-   whours=$(($RANDOM%10))
-  if [ $status -eq $(( $ispresent )) ] && [ $(($whours+$twhours)) -le 100 ] 
-  then  
-     
-     echo "employee is present";
-	dailywage=$(( $whours * $hourlywage ));  
-   	salary=$(( $salary+$dailywage ));
-     	counter=$(( $counter+1 ));
-  else  
-     echo "employee is absent";
-     salary=$(( $salary+0 ));
-  fi    
+status=$(($RANDOM%2))
+whours=$(($RANDOM%10))
+if [ $(($whours+$twhours)) -le 100 ] && [ $status -eq $(( $ispresent )) ]
+then
+		dailywage=$(( $whours * $hourlywage ))
+		salary=$(( $salary + $dailywage ))
+		day=$(($day + 1))
+			if [ $whours -lt 9 ]
+			then
+				echo "(Part-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			else
+				echo "(Full-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			fi
+			twhours=$(($twhours + $whours))
+else
+		echo "Absent."
+		day=$(($day + 1))
+fi
 done
-echo "final salary" $salary
-echo "total days present" $counter
