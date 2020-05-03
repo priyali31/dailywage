aee3afb20f2024e1644f6ff555bf6c7a67ab8a12
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-03 14:21:39 +0530

    added d1.sh

--- d1.sh
+++ d1.sh
@@ -32,3 +32,15 @@ do
 	;;
 	esac
 done
+
+status=$(( $RANDOM%2 ));
+  if [ $status -eq $(( $ispresent )) ]
+  then  
+     
+     echo "employee is present";
+     
+  else  
+     echo "employee is absent";
+     
+  fi    
+
