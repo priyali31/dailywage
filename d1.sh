1102cf568b04014445cb6db58c4a4c9fb2b04dc3
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-03 13:33:59 +0530

    added d1.sh

--- d1.sh
+++ d1.sh
@@ -1,11 +1,27 @@
 #! /bin/bash
-status=$(( $RANDOM%2 ));
-  if [ $status -eq $(( $ispresent )) ]
+ispresent=1;
+dailywage=0;
+hourlywage=20;
+salary=0;
+count=0;
+day=0
+twhours=0;
+
+for (( count=0; count<=10; count++ ))
+do
+   status=$(($RANDOM%2))
+   whours=$(($RANDOM%10))
+  if [ $status -eq $(( $ispresent )) ] && [ $(($whours+$twhours)) -le 100 ] 
   then  
      
      echo "employee is present";
- 
+	dailywage=$(( $whours * $hourlywage ));  
+   	salary=$(( $salary+$dailywage ));
+     	counter=$(( $counter+1 ));
   else  
      echo "employee is absent";
- 
+     salary=$(( $salary+0 ));
   fi    
+done
+echo "final salary" $salary
+echo "total days present" $counter
