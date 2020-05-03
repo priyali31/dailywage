a6b12814e560366d9d288609ec7c5f1c1d42a3cc
priyali31 <60377704+priyali31@users.noreply.github.com>
2020-05-03 15:28:12 +0530

    Add files via upload

---
+++ d1.sh
@@ -0,0 +1,43 @@
+#! /bin/bash
+ispresent=1;
+dailywage=0;
+hourlywage=20;
+salary=0;
+count=0;
+day=0
+twhours=0;
+echo "Welcome to Wage Computation Program"
+while [ $twhours -le 100 ]
+do
+	status=$(($RANDOM%2))
+	case $status in
+	1)	whours=$((RANDOM%10))
+		if [ $(($whours+$twhours)) -le 100 ]
+		then
+		dailywage=$(( $whours * $hourlywage ))
+		salary=$(( $salary + $dailywage ))
+		day=$(($day + 1))
+			if [ $whours -lt 9 ]
+			then
+				echo "(Part-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			else
+				echo "(Full-Time) : Present : Day-$day : $dailywage = $whours x $hourlywage"
+			fi
+			twhours=$(($twhours + $whours))
+		else
+			break
+		fi
+	;;
+	0)	echo "Absent."
+		day=$(($day + 1))
+	;;
+	esac
+done
+
+
+
+function getTotalWorkingHours(){
+	echo "Total Working Hours : $twhours"
+}
+echo  "Final Salary(Rs.) : $salary"
+getTotalWorkingHours
